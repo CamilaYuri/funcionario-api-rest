dee822ed61658dd3372bab1242dd970672c07bec
Camila Yuri Miyashiro <mila.miya@outlook.com>
2022-08-04 11:12:54 -0300

    add MainContext in AR.Infra/Data and install package of EntityFramework

---
+++ AR.Infrastructure/Data/MainContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AR.Infrastructure.Data
+{
+    public class MainContext : DbContext
+    {
+        public MainContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        //public DbSet<FuncionarioCLT> FuncionarioCLT { get; set; }
+        //public DbSet<FuncionarioPJ> FuncionarioPJ { get; set; }
+    }
+}
